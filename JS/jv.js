35acbc92184a256aa59e7e9aafcb699bcd051e6b
Nicomax0279 <92488330+Nicomax0279@users.noreply.github.com>
2021-10-13 19:35:25 -0300

    tp

---
+++ JS/jv.js
@@ -0,0 +1,22 @@
+
+function facebook(){
+alert("abrir facebook");
+
+}
+function cerrar(){
+    var p = document.getElementById("muestra");
+    p.hidden=true;
+    var c = document.getElementById("cerrar");
+    c.hidden=true;
+
+
+}
+function bizcochitos(){
+    
+    var p = document.getElementById("muestra");
+    p.hidden=false;
+    var c = document.getElementById("cerrar");
+    c.hidden=false;
+
+
+}
