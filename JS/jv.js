ab3591d1abcee02fd17e4c7c44cfa01c51cc9f95
Nicomax0279 <92488330+Nicomax0279@users.noreply.github.com>
2021-11-09 14:51:38 -0300

    casi
    ñp

--- JS/jv.js
+++ JS/jv.js
@@ -1,19 +1,12 @@
 
+    
+
 function facebook(){
 alert("abrir facebook");
 
 }
-function cerrar(){
-    var p = document.getElementById("muestra");
-    p.hidden=true;
-    var c = document.getElementById("cerrar");
-    c.hidden=true;
 
 
-}
-function atras(){
-
-    
 
 
-}
+
